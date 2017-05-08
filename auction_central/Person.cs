89dd92543a7e966adc1b374096fe754ae60f4547
Paul Harnack <harna100@mail.chapman.edu>
2017-05-08 12:34:43 -0700

    Creates Person class

---
+++ auction_central/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace auction_central {
+	public class Person {
+		public enum UserType {
+			Admin,
+			Bidder,
+			Nonprofit
+		}
+
+		public int UserId { get; set; }
+		public string Email { get; set; }
+		
+
+	}
+}
