6f2f61d3c0747d9036b499f4fdaa4dfdadb21971
Haley Kinoshita <kinos103@mail.chapman.edu>
2017-05-08 12:33:49 -0700

    password updates

---
+++ auction_central/DbWrap.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace auction_central
+{
+    public class DbWrap
+    {
+        public DbWrap()
+        {}
+
+        
+
+    }
+
+    ///```SELECT COUNT(*) FROM login WHERE type = @type AND emailAddress = @email AND password = @pass```
+    ///entering login type, emailaddress, and password
+
+
+}
