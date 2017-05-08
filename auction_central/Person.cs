dfd639ce6fdc08fa63ce65d4bc1f3a18efc71efc
Haley Kinoshita <kinos103@mail.chapman.edu>
2017-05-08 14:38:28 -0700

    updates person class

--- auction_central/Person.cs
+++ auction_central/Person.cs
@@ -7,18 +7,20 @@ using System.Threading.Tasks;
 namespace auction_central {
 	public class Person
     {
-		public enum UserType
+		public enum UserTypeEnum
         {
 			Admin,
 			Bidder,
 			Nonprofit
 		}
 
+        protected UserTypeEnum UserType { get; set; }
 		protected int UserId { get; set; }
 		protected string Email { get; set; }
 
-        public Person(UserType usertype, int userid, string email)
+        public Person(UserTypeEnum usertype, int userid, string email)
         {
+            this.UserType = usertype;
             this.UserId = userid;
             this.Email = email;
         }
