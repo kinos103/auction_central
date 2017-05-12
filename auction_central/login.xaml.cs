7de9a3b032aaf5cb2280af975aa089c446693523
Corey Lowe <lowe121@mail.chapman.edu>
2017-05-11 18:56:47 -0700

    fixed up some flow, changed to sign out button

--- auction_central/login.xaml.cs
+++ auction_central/login.xaml.cs
@@ -54,7 +54,6 @@ namespace auction_central
             }
         }
 
-        //TODO check connection string, error
         private void login_now(Person.UserTypeEnum type)
         {
             string email = LoginEmail.Text.Normalize().Trim();
@@ -84,6 +83,7 @@ namespace auction_central
                     break;
             }
             (Window.GetWindow(this) as MainWindow).HeaderNavBar.Visibility = Visibility.Visible;
+            (Window.GetWindow(this) as MainWindow).MainContent.NavigationUIVisibility = NavigationUIVisibility.Visible;
         }
 
         //click textbox and remvoe text before typing
