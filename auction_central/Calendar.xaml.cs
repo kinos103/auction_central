096c0008bc4ff29bc453f0bab76d5df4a6bca1fd
Paul Harnack <harna100@mail.chapman.edu>
2017-05-12 13:48:17 -0700

    Comments Calendar

--- auction_central/Calendar.xaml.cs
+++ auction_central/Calendar.xaml.cs
@@ -33,7 +33,7 @@ namespace auction_central
 
             DbWrap dbWrap = new DbWrap();
 
-            // TODO get list of auctions from dbWrap instead of temp data
+            // temp data for testing if db is being wonky
             /*List<Auction> tempAuctionList = new List<Auction>();
             for (int i = 0; i < 5; ++i) {
                 Auction temp = new Auction();
@@ -47,30 +47,40 @@ namespace auction_central
             temp2.StartTime = DateTime.Now.AddMonths(1);
             tempAuctionList.Add(temp2);*/
 
+
+
+            // Get list of all auctions from the database
             List<Auction> tempAuctionList = dbWrap.AuctionObjList();
 
 
 
-
+            // Take the list from database and convert it to a dictionary
+            // keys are dateTimes for a day
+            // values are a list of auction for that day
             auctions = ConvertListToDict(tempAuctionList);
 
+
+            // Add all the auctions returned from the db to all the calendars
             AddAuctionsToCalendars(CalendarSingleMonth, CalendarThreeMonthFirst, CalendarThreeMonthSecond, CalendarThreeMonthThird);
 
-            CalendarSingleMonth.SelectedDatesChanged += OnDisplayDateChange;
-            CalendarThreeMonthFirst.SelectedDatesChanged += OnDisplayDateChange;
-            CalendarThreeMonthSecond.SelectedDatesChanged += OnDisplayDateChange;
-            CalendarThreeMonthThird.SelectedDatesChanged += OnDisplayDateChange;
 
-            CalendarSingleMonth.DisplayDateChanged += CalendarThreeMonthFirst_OnSelectedDatesChanged;
-            CalendarThreeMonthFirst.DisplayDateChanged += CalendarThreeMonthFirst_OnSelectedDatesChanged;
-            CalendarThreeMonthSecond.DisplayDateChanged += CalendarThreeMonthFirst_OnSelectedDatesChanged;
-            CalendarThreeMonthThird.DisplayDateChanged += CalendarThreeMonthFirst_OnSelectedDatesChanged;
+            // Add event handlers to all the calendars
+            CalendarSingleMonth.SelectedDatesChanged += OnSelectedDatesChanged;
+            CalendarThreeMonthFirst.SelectedDatesChanged += OnSelectedDatesChanged;
+            CalendarThreeMonthSecond.SelectedDatesChanged += OnSelectedDatesChanged;
+            CalendarThreeMonthThird.SelectedDatesChanged += OnSelectedDatesChanged;
+
+            CalendarSingleMonth.DisplayDateChanged += OnDisplayDateChanged;
+            CalendarThreeMonthFirst.DisplayDateChanged += OnDisplayDateChanged;
+            CalendarThreeMonthSecond.DisplayDateChanged += OnDisplayDateChanged;
+            CalendarThreeMonthThird.DisplayDateChanged += OnDisplayDateChanged;
 
 
         }
 
         private void AddAuctionsToCalendars(params System.Windows.Controls.Calendar[] calendarsToUpdate) {
-
+            // go through each calendar passed in params
+            // highlight (select) each day that has an auction on it
             foreach (System.Windows.Controls.Calendar calendar in calendarsToUpdate) {
                 calendar.SelectedDates.Add(DateTime.Now);
                 foreach (KeyValuePair<DateTime, List<Auction>> auction in auctions) {
@@ -83,10 +93,11 @@ namespace auction_central
         private Dictionary<DateTime, List<Auction>> ConvertListToDict(List<Auction> auctionList) {
             Dictionary<DateTime, List<Auction>> toReturn = new Dictionary<DateTime, List<Auction>>();
             foreach (var auction in auctionList) {
+                // if it already has the key append to the list
                 if (toReturn.ContainsKey(auction.StartTime.Date)) {
                     toReturn[auction.StartTime.Date].Add(auction);
                 }
-                else {
+                else {// otherwise create a list with the current auction
                     List<Auction> newList = new List<Auction> {auction};
                     toReturn.Add(auction.StartTime.Date, newList);
                 }
@@ -95,6 +106,7 @@ namespace auction_central
             return toReturn;
         }
 
+        // sets the initial date to today
         private void SetCalendarDates() {
             DateTime date = DateTime.Now;
 
@@ -106,54 +118,58 @@ namespace auction_central
         }
 
 
-
-        private void CalendarThreeMonthFirst_OnSelectedDatesChanged(object sender, CalendarDateChangedEventArgs calendarDateChangedEventArgs) {
-//            AddAuctionsToCalendars(CalendarSingleMonth,CalendarThreeMonthFirst, CalendarThreeMonthSecond, CalendarThreeMonthThird);
+        // switches the 3 calendars whenever one of them change
+        // remove the handler and add them back so that it doesn't recurse
+        private void OnDisplayDateChanged(object sender, CalendarDateChangedEventArgs calendarDateChangedEventArgs) {
             if (Equals(sender, CalendarThreeMonthFirst)) {
-                CalendarThreeMonthSecond.DisplayDateChanged -= CalendarThreeMonthFirst_OnSelectedDatesChanged;
-                CalendarThreeMonthThird.DisplayDateChanged -= CalendarThreeMonthFirst_OnSelectedDatesChanged;
+                CalendarThreeMonthSecond.DisplayDateChanged -= OnDisplayDateChanged;
+                CalendarThreeMonthThird.DisplayDateChanged -= OnDisplayDateChanged;
 
                 CalendarThreeMonthSecond.DisplayDate = CalendarThreeMonthFirst.DisplayDate.AddMonths(1);
                 CalendarThreeMonthThird.DisplayDate = CalendarThreeMonthFirst.DisplayDate.AddMonths(2);
 
-                CalendarThreeMonthSecond.DisplayDateChanged += CalendarThreeMonthFirst_OnSelectedDatesChanged;
-                CalendarThreeMonthThird.DisplayDateChanged += CalendarThreeMonthFirst_OnSelectedDatesChanged;
+                CalendarThreeMonthSecond.DisplayDateChanged += OnDisplayDateChanged;
+                CalendarThreeMonthThird.DisplayDateChanged += OnDisplayDateChanged;
 
             }
             else if (Equals(sender, CalendarThreeMonthSecond)) {
-                CalendarThreeMonthFirst.DisplayDateChanged -= CalendarThreeMonthFirst_OnSelectedDatesChanged;
-                CalendarThreeMonthThird.DisplayDateChanged -= CalendarThreeMonthFirst_OnSelectedDatesChanged;
+                CalendarThreeMonthFirst.DisplayDateChanged -= OnDisplayDateChanged;
+                CalendarThreeMonthThird.DisplayDateChanged -= OnDisplayDateChanged;
 
                 CalendarThreeMonthFirst.DisplayDate = CalendarThreeMonthSecond.DisplayDate.AddMonths(-1);
                 CalendarThreeMonthThird.DisplayDate = CalendarThreeMonthSecond.DisplayDate.AddMonths(1);
 
 
-                CalendarThreeMonthFirst.DisplayDateChanged += CalendarThreeMonthFirst_OnSelectedDatesChanged;
-                CalendarThreeMonthThird.DisplayDateChanged += CalendarThreeMonthFirst_OnSelectedDatesChanged;
+                CalendarThreeMonthFirst.DisplayDateChanged += OnDisplayDateChanged;
+                CalendarThreeMonthThird.DisplayDateChanged += OnDisplayDateChanged;
             }
             else if (Equals(sender, CalendarThreeMonthThird)) {
-                CalendarThreeMonthFirst.DisplayDateChanged -= CalendarThreeMonthFirst_OnSelectedDatesChanged;
-                CalendarThreeMonthSecond.DisplayDateChanged -= CalendarThreeMonthFirst_OnSelectedDatesChanged;
+                CalendarThreeMonthFirst.DisplayDateChanged -= OnDisplayDateChanged;
+                CalendarThreeMonthSecond.DisplayDateChanged -= OnDisplayDateChanged;
 
                 CalendarThreeMonthFirst.DisplayDate = CalendarThreeMonthThird.DisplayDate.AddMonths(-2);
                 CalendarThreeMonthSecond.DisplayDate = CalendarThreeMonthThird.DisplayDate.AddMonths(-1);
 
-                CalendarThreeMonthFirst.DisplayDateChanged += CalendarThreeMonthFirst_OnSelectedDatesChanged;
-                CalendarThreeMonthSecond.DisplayDateChanged += CalendarThreeMonthFirst_OnSelectedDatesChanged;
+                CalendarThreeMonthFirst.DisplayDateChanged += OnDisplayDateChanged;
+                CalendarThreeMonthSecond.DisplayDateChanged += OnDisplayDateChanged;
             }
         }
 
-        private void OnDisplayDateChange(object sender, SelectionChangedEventArgs selectionChangedEventArgs) {
-            System.Windows.Controls.Calendar calendarSent = sender as System.Windows.Controls.Calendar;
-            Console.WriteLine(calendarSent);
-//            DateTime selected = calendarSent.SelectedDate.Value;
-            DateTime selected;
-	        DateTime.TryParse(calendarSent.ToString(), out selected);
-            calendarSent.SelectedDates.Remove(selected);
-            calendarSent.SelectedDatesChanged -= OnDisplayDateChange;
-            AddAuctionsToCalendars(calendarSent);
-            calendarSent.SelectedDatesChanged += OnDisplayDateChange;
 
+        private void OnSelectedDatesChanged(object sender, SelectionChangedEventArgs selectionChangedEventArgs) {
+            // Get the calendar
+            System.Windows.Controls.Calendar calendarSent = sender as System.Windows.Controls.Calendar;
+            DateTime selected;
+            // had issues with calendarSent.Date.Value
+	        DateTime.TryParse(calendarSent.ToString(), out selected);
+            // remove the one they just selected
+            calendarSent.SelectedDates.Remove(selected);
+            calendarSent.SelectedDatesChanged -= OnSelectedDatesChanged;
+            // readd the auctions
+            AddAuctionsToCalendars(calendarSent);
+            calendarSent.SelectedDatesChanged += OnSelectedDatesChanged;
+
+            // if they clicked a day that has an auction display the dialog
             if (auctions.ContainsKey(selected.Date))
             {
                 ListBoxDialog.ItemsSource = auctions[selected];
@@ -164,12 +180,14 @@ namespace auction_central
             }
         }
 
+        // allows the scrollViewer to scroll
         private void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e) {
             ScrollViewer scv = (ScrollViewer)sender;
             scv.ScrollToVerticalOffset(scv.VerticalOffset - e.Delta);
             e.Handled = true;
         }
 
+        // get the auction from the listbox and then navigate
         private void ListBoxDialog_OnMouseUp(object sender, MouseButtonEventArgs e) {
             Auction selected = ListBoxDialog.SelectedValue as Auction;
 
