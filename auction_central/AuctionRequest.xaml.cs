1ed7822af9da9abd75e4e85b441f48ab6c6a1ac5
Paul Harnack <harna100@mail.chapman.edu>
2017-05-14 22:18:51 -0700

    Starts validating AuctionRequest

--- auction_central/AuctionRequest.xaml.cs
+++ auction_central/AuctionRequest.xaml.cs
@@ -25,10 +25,22 @@ namespace auction_central
             InitializeComponent();
         }
 
+
+        public bool Validate() {
+            
+
+            return false;
+        }
         //must make sure that request is assigned to nonprofitID
 
         private void placeRequest_Click(object sender, RoutedEventArgs e)
         {
+
+            if (!Validate()) {
+                MessageBox.Show("One or more fields are empty");
+                return;
+            }
+
             string charityName = this.charityName.Text;
 
             //contactID
@@ -41,14 +53,20 @@ namespace auction_central
             //dateTimeID
             //should auction date be string? ex: May 10, 2017. See what works best with calendar
             int auctionDate = Int32.Parse(this.auctionDate.Text);
-            string startTime = this.startTime.Text;
-            string endTime = this.endTime.Text;
+//            string startTime = this.startTime.Text;
+//            string endTime = this.endTime.Text;
+            DateTime startTime = DateTime.Parse(this.startTime.Text);
+            DateTime endTime = DateTime.Parse(this.endTime.Text);
 
             //check if numOfItems and additionalComments are on auctioninfo table
             int numOfItems = Int32.Parse(this.numOfItems.Text);
             string additionalComments = this.additionalComments.Text;
 
+            NonProfit npObj = (Window.GetWindow(this) as MainWindow).User as NonProfit;
 
+            Auction toCreate = new Auction(charityName, startTime, endTime, npObj.FirstName + " " + npObj.LastName, phoneNumber.ToString());
+
+            new DbWrap().InsertAuction(toCreate, npObj, phoneNumber);
         }
     }
 }
