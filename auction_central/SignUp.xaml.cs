b23a2559e8136c8081b8877dd3cb243d8b1bf2a3
Corey Lowe <lowe121@mail.chapman.edu>
2017-05-15 00:12:55 -0700

    even better sign up validation

--- auction_central/SignUp.xaml.cs
+++ auction_central/SignUp.xaml.cs
@@ -120,14 +120,16 @@ namespace auction_central
             {
                 string streetAddress = SignUpAddress.Text;
                 string city = SignUpCity.Text;
-                int zipcode = Int32.Parse(SignUpZipcode.Text);
+                int zipcode;
+                Int32.TryParse(SignUpZipcode.Text, out zipcode);
                 string state = SignUpState.Text;
                 string creditCardNum = SignUpCreditCard.Text;
-                int cvv = Int32.Parse(SignUpCVV.Text);
+                int cvv;
+                Int32.TryParse(SignUpCVV.Text, out cvv);
                 string exp = SignUpCreditCardExpDate.Text;
 
                 if (streetAddress == "" || city == "" || state == "" || creditCardNum == "" || exp == "" || 
-                    zipcode.ToString() == "" || cvv.ToString() == "" || cvv.ToString().Length > 3 || creditCardNum.Length != 16)
+                    zipcode == 0 || cvv == 0 || cvv.ToString().Length < 3 || creditCardNum.Length != 16 || cvv.ToString().Length > 4)
                 {
                     MessageBox.Show("Error creating bidder: check credit card and address");
                     return;
