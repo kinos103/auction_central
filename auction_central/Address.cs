283c2e4e8cc3f41b402db9a569754bcdcac1e888
Paul Harnack <harna100@mail.chapman.edu>
2017-05-14 20:02:18 -0700

    Creates Address and CreditCard classes

---
+++ auction_central/Address.cs
@@ -0,0 +1,15 @@
+﻿namespace auction_central {
+    public class Address {
+        public string HouseNumber {get; set;}
+        public string City {get; set;}
+        public string State {get; set;}
+        public int Zipcode {get; set;}
+
+        public Address(string houseNumber, string city, string state, int zipcode) {
+            HouseNumber = houseNumber;
+            City = city;
+            State = state;
+            Zipcode = zipcode;
+        }
+    }
+}
---
+++ auction_central/CreditCard.cs
@@ -0,0 +1,15 @@
+﻿namespace auction_central {
+    public class CreditCard {
+        public string CardNumber {get; set;}
+        public int CVV {get; set;}
+        public string NameOnCard{get; set;}
+        public string ExpDate{get; set;}
+
+        public CreditCard(string cardNumber, int cvv, string nameOnCard, string expDate) {
+            CardNumber = cardNumber;
+            CVV = cvv;
+            NameOnCard = nameOnCard;
+            ExpDate = expDate;
+        }
+    }
+}
