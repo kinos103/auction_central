f23fa09dd7fadd64d33ef9aaea88d18084c3b007
Corey Lowe <lowe121@mail.chapman.edu>
2017-05-15 00:55:18 -0700

    np event details information

--- auction_central/NPEventDetails.xaml.cs
+++ auction_central/NPEventDetails.xaml.cs
@@ -20,9 +20,66 @@ namespace auction_central
     /// </summary>
     public partial class NPEventDetails : Page
     {
+
+        private List<Auction> allAuctions;
+        private List<Auction> upComing;
+        private List<Auction> belongsToNP;
+        private NonProfit npObj;
         public NPEventDetails()
         {
             InitializeComponent();
+            allAuctions = new DbWrap().AuctionObjList();
+            upComing = new List<Auction>();
+            belongsToNP = new List<Auction>();
+
+            this.Loaded += (sender, args) => {
+                npObj = (Window.GetWindow(this) as MainWindow).User as NonProfit;
+                AddUpcoming();
+                AddBelongsTo();
+
+                if (belongsToNP.Count != 0)
+                {
+                    AuctionID.Text += ": " + belongsToNP[0].CharityName + belongsToNP[0].EventDate.Month + belongsToNP[0].EventDate.Day + belongsToNP[0].EventDate.Year;
+                    AuctionDate.Text += ": " + belongsToNP[0].EventDate.ToShortDateString();
+                    CharityName.Text += ": " + belongsToNP[0].CharityName;
+                    StartTime.Text += ": " + belongsToNP[0].StartTime.ToShortTimeString();
+                    EndTime.Text += ": " + belongsToNP[0].EndTime.ToShortTimeString();
+                    Location.Text += ": " + belongsToNP[0].Location;
+                }
+
+                NoAuction.Visibility = belongsToNP.Count == 0 ? Visibility.Visible : Visibility.Collapsed;
+                AuctionID.Visibility = belongsToNP.Count != 0 ? Visibility.Visible : Visibility.Collapsed;
+                AuctionDate.Visibility = belongsToNP.Count != 0 ? Visibility.Visible : Visibility.Collapsed;
+                CharityName.Visibility = belongsToNP.Count != 0 ? Visibility.Visible : Visibility.Collapsed;
+                StartTime.Visibility = belongsToNP.Count != 0 ? Visibility.Visible : Visibility.Collapsed;
+                Location.Visibility = belongsToNP.Count != 0 ? Visibility.Visible : Visibility.Collapsed;
+                EndTime.Visibility = belongsToNP.Count != 0 ? Visibility.Visible : Visibility.Collapsed;
+                addItemsButton.Visibility = belongsToNP.Count != 0 ? Visibility.Visible : Visibility.Collapsed;
+                ViewItemsButton.Visibility = belongsToNP.Count != 0 ? Visibility.Visible : Visibility.Collapsed;
+            };
+
+        }
+
+        private void AddBelongsTo()
+        {
+            foreach (Auction auction in upComing)
+            {
+                if (auction.CharityName == npObj.OrgName)
+                {
+                    belongsToNP.Add(auction);
+                }
+            }
+        }
+
+        private void AddUpcoming()
+        {
+            foreach (Auction auction in allAuctions)
+            {
+                if (auction.EventDate >= DateTime.Now)
+                {
+                    upComing.Add(auction);
+                }
+            }
         }
 
         private void addItemsButton_Click(object sender, RoutedEventArgs e)
