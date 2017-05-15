5f06077ea1ff1cf65627576bc551401a71953130
Isabella Cedric <cedri100@mail.chapman.edu>
2017-05-14 22:13:48 -0700

    input valiation, date picker

--- auction_central/AuctionRequest.xaml.cs
+++ auction_central/AuctionRequest.xaml.cs
@@ -25,10 +25,78 @@ namespace auction_central
             InitializeComponent();
         }
 
+       private bool Validate()
+        {
+            int intPhoneNumber;
+            //int intAuctionDate;
+            int intNumOfItems;
+
+            if (string.IsNullOrEmpty(charityName.Text))
+            {
+                MessageBox.Show("Please enter Charity Name");
+            }
+
+            else if(string.IsNullOrEmpty(firstName.Text))
+            {
+                MessageBox.Show("Please enter your first name");
+            }
+
+            else if(string.IsNullOrEmpty(lastName.Text))
+            {
+                MessageBox.Show("Please enter your last name");
+            }
+            
+            else if (!int.TryParse(phoneNumber.Text, out intPhoneNumber))
+            {
+                MessageBox.Show("Please enter phone number");
+            }
+
+            else if(string.IsNullOrEmpty(location.Text))
+            {
+                MessageBox.Show("Please enter location");
+            }
+
+            else if (string.IsNullOrEmpty(startTime.Text))
+            {
+                MessageBox.Show("Please enter start time for the event");
+            }
+
+            else if (string.IsNullOrEmpty(endTime.Text))
+            {
+                MessageBox.Show("Please enter end time for the event");
+            }
+
+            else if (!int.TryParse(numOfItems.Text, out intNumOfItems))
+            {
+                MessageBox.Show("Please enter number of items included in the bundle");
+            }
+
+            else
+            {
+                MessageBox.Show("Error sending in Auction Request");
+            }
+
+            return true;
+        }
+
         //must make sure that request is assigned to nonprofitID
+
+        private void DatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var dateString = DatePicker1.SelectedDate.Value.ToShortDateString();
+
+            DatePicker1.SelectedDate = DateTime.Parse(dateString);
+             
+        }
 
         private void placeRequest_Click(object sender, RoutedEventArgs e)
         {
+            if (!Validate())
+            {
+                MessageBox.Show("One or more fields are wrong");
+                return;
+            }
+
             string charityName = this.charityName.Text;
 
             //contactID
@@ -40,7 +108,7 @@ namespace auction_central
 
             //dateTimeID
             //should auction date be string? ex: May 10, 2017. See what works best with calendar
-            int auctionDate = Int32.Parse(this.auctionDate.Text);
+            //int auctionDate = Int32.Parse(this.auctionDate.Text);
             string startTime = this.startTime.Text;
             string endTime = this.endTime.Text;
 
@@ -50,5 +118,6 @@ namespace auction_central
 
 
         }
+
     }
 }
